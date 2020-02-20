ca5fdf24c53ab4407b193a26abf9ec5af5c5b82f
dmyronchuk <dmyronchuk@mint-innovations.com>
2020-02-20 16:20:19 +0200

    first commit init

---
+++ build/precache-manifest.6515027472735ba19d859486164a8b5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "711c080254b158665fff1528c08f618d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4289962340ee7bea1c2",
+    "url": "/static/css/main.aa66161c.chunk.css"
+  },
+  {
+    "revision": "aa61609b16b16c65b12f",
+    "url": "/static/js/2.792a68ee.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.792a68ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4289962340ee7bea1c2",
+    "url": "/static/js/main.b4d302b2.chunk.js"
+  },
+  {
+    "revision": "48ec9aeec1416df7a02f",
+    "url": "/static/js/runtime-main.6fedbad4.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import BlockUser from "./BlockUser/BlockUser";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    state = {
+        users: [
+            { name: 'Gregory', surname: 'Point', age: Math.round(Math.random() * 100)},
+            { name: 'Tomas', surname: 'Tobi', age: Math.round(Math.random() * 100)},
+            { name: 'Victoria', surname: 'Holli', age: Math.round(Math.random() * 100)},
+            { name: 'Seiko', surname: 'Zero', age: Math.round(Math.random() * 100)},
+            { name: 'Mia', surname: 'Loop', age: Math.round(Math.random() * 100)}
+        ]
+    }
+
+    render() {
+        let users = this.state.users
+
+        return (
+            <div className="app">
+                <BlockUser name={users[0].name} surname={users[0].surname} age={users[0].age} />
+                <BlockUser name={users[1].name} surname={users[1].surname} age={users[1].age} />
+                <BlockUser name={users[2].name} surname={users[2].surname} age={users[2].age} />
+                <BlockUser name={users[3].name} surname={users[3].surname} age={users[3].age} />
+                <BlockUser name={users[4].name} surname={users[4].surname} age={users[4].age} />
+            </div>
+        )
+    }
 }
 
 export default App;
